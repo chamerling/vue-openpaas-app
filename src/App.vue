ec29822cd68fbc4969dcbc67b467e01eb5d0e6f1
Christophe Hamerling <chamerling@linagora.com>
2018-09-28 14:23:23 +0200

    Night mode a la twitter

--- src/App.vue
+++ src/App.vue
@@ -17,14 +17,6 @@
               <v-list-tile-title>Settings</v-list-tile-title>
             </v-list-tile-content>
           </v-list-tile>
-          <v-list-tile>
-            <v-list-tile-action>
-              <v-switch v-model="dark"/>
-            </v-list-tile-action>
-            <v-list-tile-content>
-              <v-list-tile-title>Dark Mode</v-list-tile-title>
-            </v-list-tile-content>
-          </v-list-tile>
         </v-list>
       </v-navigation-drawer>
       <v-toolbar
@@ -56,7 +48,16 @@
           </v-avatar>
           <v-list>
             <v-list-tile @click.prevent="logout">
-              <v-list-tile-title>Logout</v-list-tile-title>
+              <v-list-tile-title>Log out</v-list-tile-title>
+            </v-list-tile>
+            <v-divider/>
+            <v-list-tile avatar @click.prevent="nightMode">
+              <v-list-tile-content>
+              <v-list-tile-title>Night mode</v-list-tile-title>
+            </v-list-tile-content>
+              <v-list-tile-action>
+                <v-icon id="moon" color="grey lighten-1">brightness_2</v-icon>
+            </v-list-tile-action>
             </v-list-tile>
           </v-list>
         </v-menu>
@@ -96,6 +97,10 @@ export default {
       this.$store.dispatch('logout').then(() => {
         this.$router.push({name: 'Login'});
       });
+    },
+
+    nightMode() {
+      this.dark = !this.dark;
     }
   },
   computed: {
@@ -112,4 +117,7 @@ export default {
   #header-logo
     height: 35px;
     width: 150px;
+
+  #moon
+    transform: rotate(150deg)
 </style>
