533e9bbaf34e6770c3585db08b6f286830eb14fe
Christophe Hamerling <chamerling@linagora.com>
2018-10-01 08:32:51 +0200

    Rework user menu

--- src/App.vue
+++ src/App.vue
@@ -47,6 +47,20 @@
             <v-icon v-else>account_circle</v-icon>
           </v-avatar>
           <v-list>
+            <v-list-tile>
+              <v-list-tile-content>
+              <v-list-tile-title class="title">{{getUserName}}</v-list-tile-title>
+              <v-list-tile-sub-title>{{getUserEmail}}</v-list-tile-sub-title>
+              </v-list-tile-content>
+            </v-list-tile>
+            <v-divider/>
+            <v-list-tile @click.prevent="">
+              <v-list-tile-title>Profile</v-list-tile-title>
+            </v-list-tile>
+            <v-divider/>
+            <v-list-tile @click.prevent="">
+              <v-list-tile-title>Settings</v-list-tile-title>
+            </v-list-tile>
             <v-list-tile @click.prevent="logout">
               <v-list-tile-title>Log out</v-list-tile-title>
             </v-list-tile>
@@ -104,7 +118,8 @@ export default {
     }
   },
   computed: {
-    ...mapGetters(['getUserAvatarUrl'])
+    ...mapGetters(['getUserAvatarUrl', 'getUserName', 'getUserEmail']),
+    color() {
   },
   components: {
     'applications-menu': ApplicationsMenu,
--- src/store/getters.js
+++ src/store/getters.js
@@ -1,3 +1,5 @@
 export const getUserAvatarUrl = state => state.session.user && state.session.user._id && `${state.baseUrl}/api/users/${state.session.user._id}/profile/avatar`;
 
 export const getUserName = state => state.session.user && `${state.session.user.firstname} ${state.session.user.lastname}`;
+
+export const getUserEmail = state => state.session.user && state.session.user.preferredEmail;
